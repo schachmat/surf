b6f17c723c00d454da72ac6e1420302c3127166f
Markus Teich <markus.teich@stusta.mhn.de>
2013-12-28 14:54:23 +0100

    add searchengines patch

--- config.def.h
+++ config.def.h
@@ -49,54 +49,62 @@ static Bool allowgeolocation = TRUE;
 
 #define MODKEY GDK_CONTROL_MASK
 
+// searchengines
+static SearchEngine searchengines[] = {
+	{ "g",        "http://www.google.de/search?q=%s"   },
+	{ "en",       "http://dict.cc/?s=%s" },
+	{ "ug",       "http://%s.uni-goettingen.de" },
+	{ "dict",     "http://www.thefreedictionary.com/%s" },
+};
+
 /* hotkeys */
 /*
  * If you use anything else but MODKEY and GDK_SHIFT_MASK, don't forget to
  * edit the CLEANMASK() macro.
  */
 static Key keys[] = {
-    /* modifier	            keyval      function    arg             Focus */
-    { MODKEY|GDK_SHIFT_MASK,GDK_r,      reload,     { .b = TRUE } },
-    { MODKEY,               GDK_r,      reload,     { .b = FALSE } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_p,      print,      { 0 } },
+	/* modifier	            keyval      function    arg             Focus */
+	{ MODKEY|GDK_SHIFT_MASK,GDK_r,      reload,     { .b = TRUE } },
+	{ MODKEY,               GDK_r,      reload,     { .b = FALSE } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_p,      print,      { 0 } },
 
-    { MODKEY,               GDK_p,      clipboard,  { .b = TRUE } },
-    { MODKEY,               GDK_y,      clipboard,  { .b = FALSE } },
+	{ MODKEY,               GDK_p,      clipboard,  { .b = TRUE } },
+	{ MODKEY,               GDK_y,      clipboard,  { .b = FALSE } },
 
-    { MODKEY|GDK_SHIFT_MASK,GDK_j,      zoom,       { .i = -1 } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_k,      zoom,       { .i = +1 } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_q,      zoom,       { .i = 0  } },
-    { MODKEY,               GDK_minus,  zoom,       { .i = -1 } },
-    { MODKEY,               GDK_plus,   zoom,       { .i = +1 } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_j,      zoom,       { .i = -1 } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_k,      zoom,       { .i = +1 } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_q,      zoom,       { .i = 0  } },
+	{ MODKEY,               GDK_minus,  zoom,       { .i = -1 } },
+	{ MODKEY,               GDK_plus,   zoom,       { .i = +1 } },
 
-    { MODKEY,               GDK_l,      navigate,   { .i = +1 } },
-    { MODKEY,               GDK_h,      navigate,   { .i = -1 } },
+	{ MODKEY,               GDK_l,      navigate,   { .i = +1 } },
+	{ MODKEY,               GDK_h,      navigate,   { .i = -1 } },
 
-    { MODKEY,               GDK_j,           scroll_v,   { .i = +1 } },
-    { MODKEY,               GDK_k,           scroll_v,   { .i = -1 } },
-    { MODKEY,               GDK_b,           scroll_v,   { .i = -10000 } },
-    { MODKEY,               GDK_space,       scroll_v,   { .i = +10000 } },
-    { MODKEY,               GDK_i,           scroll_h,   { .i = +1 } },
-    { MODKEY,               GDK_u,           scroll_h,   { .i = -1 } },
+	{ MODKEY,               GDK_j,           scroll_v,   { .i = +1 } },
+	{ MODKEY,               GDK_k,           scroll_v,   { .i = -1 } },
+	{ MODKEY,               GDK_b,           scroll_v,   { .i = -10000 } },
+	{ MODKEY,               GDK_space,       scroll_v,   { .i = +10000 } },
+	{ MODKEY,               GDK_i,           scroll_h,   { .i = +1 } },
+	{ MODKEY,               GDK_u,           scroll_h,   { .i = -1 } },
 
-    { 0,                    GDK_F11,    fullscreen, { 0 } },
-    { 0,                    GDK_Escape, stop,       { 0 } },
-    { MODKEY,               GDK_o,      source,     { 0 } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_o,      inspector,  { 0 } },
+	{ 0,                    GDK_F11,    fullscreen, { 0 } },
+	{ 0,                    GDK_Escape, stop,       { 0 } },
+	{ MODKEY,               GDK_o,      source,     { 0 } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_o,      inspector,  { 0 } },
 
-    { MODKEY,               GDK_g,      spawn,      SETPROP("_SURF_URI", "_SURF_GO") },
-    { MODKEY,               GDK_f,      spawn,      SETPROP("_SURF_FIND", "_SURF_FIND") },
-    { MODKEY,               GDK_slash,  spawn,      SETPROP("_SURF_FIND", "_SURF_FIND") },
+	{ MODKEY,               GDK_g,      spawn,      SETPROP("_SURF_URI", "_SURF_GO") },
+	{ MODKEY,               GDK_f,      spawn,      SETPROP("_SURF_FIND", "_SURF_FIND") },
+	{ MODKEY,               GDK_slash,  spawn,      SETPROP("_SURF_FIND", "_SURF_FIND") },
 
-    { MODKEY,               GDK_n,      find,       { .b = TRUE } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_n,      find,       { .b = FALSE } },
+	{ MODKEY,               GDK_n,      find,       { .b = TRUE } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_n,      find,       { .b = FALSE } },
 
-    { MODKEY|GDK_SHIFT_MASK,GDK_c,      toggle,     { .v = "enable-caret-browsing" } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_i,      toggle,     { .v = "auto-load-images" } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_s,      toggle,     { .v = "enable-scripts" } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_v,      toggle,     { .v = "enable-plugins" } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_m,      togglestyle,{ 0 } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_b,      togglescrollbars,{ 0 } },
-    { MODKEY|GDK_SHIFT_MASK,GDK_g,      togglegeolocation, { 0 } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_c,      toggle,     { .v = "enable-caret-browsing" } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_i,      toggle,     { .v = "auto-load-images" } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_s,      toggle,     { .v = "enable-scripts" } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_v,      toggle,     { .v = "enable-plugins" } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_m,      togglestyle,{ 0 } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_b,      togglescrollbars,{ 0 } },
+	{ MODKEY|GDK_SHIFT_MASK,GDK_g,      togglegeolocation, { 0 } },
 };
 
